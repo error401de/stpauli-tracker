5198f3535f4ba67fe6b06c840fb71aea7231c355
error401de <github@anroba.de>
2022-10-01 17:44:00 +0200

    next games box solves #7

--- js/getdata.js
+++ js/getdata.js
@@ -1,7 +1,59 @@
+const currentYear = new Date().getFullYear()
+
 async function callApi(url) {
 	let response = await fetch(url);
 	let data = await response.json();
 	return data;
+}
+
+function getDfbPokalId(data) {
+	dfbPokalId = data[0]['LeagueId'];
+	callApi('https://www.openligadb.de/api/getnextmatchbyleagueteam/' + dfbPokalId + '/98').then(data => printDfbPokal(data))
+}
+
+function getBl2Id(data) {
+	bl2Id = data[0]['LeagueId'];
+	callApi('https://www.openligadb.de/api/getnextmatchbyleagueteam/' + bl2Id + '/98').then(data => printBl2(data))
+}
+
+function printDfbPokal(data) {
+	let textDfbNextGame = document.getElementById('dfbNextGame');
+	let textDfbNextGameMatchday = document.getElementById('dfbNextGameMatchday');
+	let textDfbNextGameTeam1 = document.getElementById('dfbNextGameTeam1');
+	let textDfbNextGameTeam2 = document.getElementById('dfbNextGameTeam2');
+
+	let textDfbNextGameIconTeam1 = document.getElementById('dfbNextGameIconTeam1')
+	textDfbNextGameIconTeam1.src = data['Team1']['TeamIconUrl'];
+	textDfbNextGameIconTeam1.setAttribute("class", "img-icon-big");
+
+	let textDfbNextGameIconTeam2 = document.getElementById('dfbNextGameIconTeam2')
+	textDfbNextGameIconTeam2.src = data['Team2']['TeamIconUrl'];
+	textDfbNextGameIconTeam2.setAttribute("class", "img-icon-big");
+	
+	textDfbNextGame.innerHTML = data['LeagueName'];
+	textDfbNextGameMatchday.innerHTML = data['Group']['GroupName'] + ' - ' + new Date(data['MatchDateTime']).toLocaleString('de-DE', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', });
+	textDfbNextGameTeam1.innerHTML = data['Team1']['ShortName'];
+	textDfbNextGameTeam2.innerHTML = data['Team2']['ShortName'];
+}
+
+function printBl2(data) {
+	let textBl2NextGame = document.getElementById('bl2NextGame');
+	let textBl2NextGameMatchday = document.getElementById('bl2NextGameMatchday');
+	let textBl2NextGameTeam1 = document.getElementById('bl2NextGameTeam1');
+	let textBl2NextGameTeam2 = document.getElementById('bl2NextGameTeam2');
+
+	let textBl2NextGameIconTeam1 = document.getElementById('bl2NextGameIconTeam1')
+	textBl2NextGameIconTeam1.src = data['Team1']['TeamIconUrl'];
+	textBl2NextGameIconTeam1.setAttribute("class", "img-icon-big");
+
+	let textBl2NextGameIconTeam2 = document.getElementById('bl2NextGameIconTeam2')
+	textBl2NextGameIconTeam2.src = data['Team2']['TeamIconUrl'];
+	textBl2NextGameIconTeam2.setAttribute("class", "img-icon-big");
+	
+	textBl2NextGame.innerHTML = data['LeagueName'];
+	textBl2NextGameMatchday.innerHTML = data['Group']['GroupName'] + ' - ' + new Date(data['MatchDateTime']).toLocaleString('de-DE', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', });
+	textBl2NextGameTeam1.innerHTML = data['Team1']['ShortName'];
+	textBl2NextGameTeam2.innerHTML = data['Team2']['ShortName'];
 }
 
 function printTable(data) {
@@ -22,7 +74,7 @@ function printTable(data) {
 		let textLogo = document.createElement('img')
 		textLogo.src = data[key]['TeamIconUrl']
 		textLogo.setAttribute("class", "img-icon");
-		let textTeam = document.createTextNode(data[key]['TeamName']);
+		let textTeam = document.createTextNode(data[key]['ShortName']);
 		let textMatches = document.createTextNode(data[key]['Matches']);
 		let textSUN = document.createTextNode(data[key]['Won'] + "/" + data[key]['Draw'] + "/" + data[key]['Lost']);
 		let textGoalDiff = document.createTextNode(data[key]['GoalDiff']);
@@ -36,11 +88,11 @@ function printTable(data) {
 		newCellGoalDiff.appendChild(textGoalDiff)
 		newCellPoints.appendChild(textPoints)
 
-		console.log(data[key]['TeamName']);
-
 		helper++;
 	}
 }
 
 
-callApi('https://www.openligadb.de/api/getbltable/bl2/2022').then(data => printTable(data))
+callApi('https://www.openligadb.de/api/getbltable/bl2/' + currentYear).then(data => printTable(data));
+callApi('https://www.openligadb.de/api/getmatchdata/dfb' + currentYear).then(data => getDfbPokalId(data));
+callApi('https://www.openligadb.de/api/getmatchdata/bl2').then(data => getBl2Id(data));
