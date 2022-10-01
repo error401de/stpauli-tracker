119b5fb97a4a56b69e31ff0e5c44ab61f362edfa
error401de <github@anroba.de>
2022-10-01 15:33:10 +0200

    add league table fixes #5

--- js/getdata.js
+++ js/getdata.js
@@ -1,5 +1,46 @@
-function initiate() {
-	console.log('hi')
+async function callApi(url) {
+	let response = await fetch(url);
+	let data = await response.json();
+	return data;
 }
 
-initiate()
+function printTable(data) {
+	let table = document.getElementById('leagueTable');
+	let helper = 1;
+	for (let key in data) {
+		let newRow = table.insertRow(helper);
+
+		let newCellPosition = newRow.insertCell(-1);
+		let newCellLogo= newRow.insertCell(-1);
+		let newCellTeam= newRow.insertCell(-1);
+		let newCellMatches= newRow.insertCell(-1);
+		let newCellSUN= newRow.insertCell(-1);
+		let newCellGoalDiff= newRow.insertCell(-1);
+		let newCellPoints= newRow.insertCell(-1);
+
+		let textPosition = document.createTextNode(helper);
+		let textLogo = document.createElement('img')
+		textLogo.src = data[key]['TeamIconUrl']
+		textLogo.setAttribute("class", "img-icon");
+		let textTeam = document.createTextNode(data[key]['TeamName']);
+		let textMatches = document.createTextNode(data[key]['Matches']);
+		let textSUN = document.createTextNode(data[key]['Won'] + "/" + data[key]['Draw'] + "/" + data[key]['Lost']);
+		let textGoalDiff = document.createTextNode(data[key]['GoalDiff']);
+		let textPoints = document.createTextNode(data[key]['Points']);
+
+		newCellPosition.appendChild(textPosition)
+		newCellLogo.appendChild(textLogo)
+		newCellTeam.appendChild(textTeam)
+		newCellMatches.appendChild(textMatches)
+		newCellSUN.appendChild(textSUN)
+		newCellGoalDiff.appendChild(textGoalDiff)
+		newCellPoints.appendChild(textPoints)
+
+		console.log(data[key]['TeamName']);
+
+		helper++;
+	}
+}
+
+
+callApi('https://www.openligadb.de/api/getbltable/bl2/2022').then(data => printTable(data))
